c77da2b2a6452652799327b62020e2d7f5aabf24
PetrovaEkat <katisyn853@gmail.com>
2023-05-31 14:36:06 +0400

    Выполнено ДЗ

--- src/main/resources/liquibase/scripts/index-practice.sql
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -1,7 +1,7 @@
-- liquibase formatted sql
+--liquibase formatted sql
 
-— changeset EkaterinaPetro:1
+--changeset EkaterinaPetro:1
 CREATE INDEX student_name_index ON student (name);
 
-— changeset EkaterinaPetro:2
+--changeset EkaterinaPetro:2
 CREATE INDEX faculty_name_color_index ON faculty (name, color);
